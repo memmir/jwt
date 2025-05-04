78d164ca17e08b85b753adf75bd750d0a6ca7fc4
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2025-05-04 13:31:14 +0300

    JWT service created.

---
+++ src/main/java/com/security/jwt/jwtbusiness/JwtService.java
@@ -0,0 +1,60 @@
+package com.security.jwt.jwtbusiness;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtService {
+
+    public static final String SECRET_KEY = "c7Swu56AJiwo82rKgoQW37rgvjZCjxC8Diz++RhKDvY=";
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, String> claimsMap = new HashMap<>();
+        claimsMap.put("role", "ADMIN");
+
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setClaims(claimsMap)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60* 2))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public <T> T exportToken(String token, Function<Claims, T > claimsFunction){  // Gelen tokeni çözümlemek için kullanılan metod
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token).getBody();
+
+        return claimsFunction.apply(claims);
+    }
+
+    public String getUsernameByToken(String token){ // Tokenin içinde gelen username i almak için kullanılan metod
+        return exportToken(token, Claims::getSubject);
+    }
+
+    public boolean isTokenExpired(String token){
+        Date expiredDate = exportToken(token, Claims::getExpiration);
+
+        return new Date().before(expiredDate);
+    }
+
+    public Key getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
